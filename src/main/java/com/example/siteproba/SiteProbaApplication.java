806512ebafa1a024322454b3788db924259a9d85
Mike Ogar <ogar.m.v@mail.ru>
2023-10-12 20:27:15 +0300

    Initial commit

---
+++ src/main/java/com/example/siteproba/SiteProbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.siteproba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteProbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteProbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/siteproba/SiteProbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siteproba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteProbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
